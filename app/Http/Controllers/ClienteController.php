563be5c5f545db7db39c769b990b4b99f632977a
asdk <asdk>
2023-07-28 16:35:26 -0300

    primeira commit

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use app\Models\Cliente;
+
+class ClienteController extends Controller
+{
+    //
+}
---
+++ database/migrations/2023_07_28_185158_create_cliente_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nome');
+            $table->string('Endereço');
+            $table->string('Bairro');
+            $table->integer('Cep');
+            $table->string('Cidade');
+            $table->string('Estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cliente');
+    }
+};
